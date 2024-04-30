74889b1d866abcac8c33ed087deb32ea58db2fad
dattaaanand <dxttanand@gmail.com>
2024-04-30 11:11:03 +0530

    Added JavaScript

---
+++ navigation.js
@@ -0,0 +1,16 @@
+const ids = ["p911","p718","Panamera","Taycan","Macan","Cayenne"];
+function myfunc(buttonID){
+    if (buttonID  == "p911"){
+    } 
+    elif (buttonID  == "p911"){
+    } 
+    elif (buttonID  == "p911"){
+    } 
+    elif (buttonID  == "p911"){
+    } 
+    elif (buttonID  == "p911"){
+    } 
+    elif (buttonID  == "p911"){
+    } 
+    
+}
